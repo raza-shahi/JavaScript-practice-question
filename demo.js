372fc91b3eeabc65ba0a3a40250ab2ba1b9b396c
Shahid <109452231+raza-shahi@users.noreply.github.com>
2026-05-18 12:06:01 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,24 @@
+function diamondPrint(n){
+  for(let i=1;i<=n;i+=2){
+    let UpperStar = "";
+    for(let j=1;j<=(n-i)/2;j++){
+      UpperStar += "  ";
+    }
+    for(let k=1;k<=i;k++){
+      UpperStar += "* ";
+    }
+    console.log(UpperStar);
+  }
+  for(let i=n-2;i>=1;i-=2){
+    let LowerStar = "";
+    for(let j=1;j<=(n-i)/2;j++){
+      LowerStar += "  ";
+    }
+    for(let k=1;k<=i;k++){
+      LowerStar += "* ";
+    }
+    console.log(LowerStar);
+  }
+}
+let num = 5;
+diamondPrint(num)
